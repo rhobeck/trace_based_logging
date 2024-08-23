75ed8048f9bd57de2f98ef1738c839d521df3cdb
lowyyy <38222751+tomfunke@users.noreply.github.com>
2024-08-23 12:11:25 +0200

    fix last commit: cant convert address if there are no events

--- trace_decoder/data_preparation.py
+++ trace_decoder/data_preparation.py
@@ -382,7 +382,8 @@ def decode_events(df_log, dict_abi):
     del df_events_raw
     
     df_events = pd.DataFrame(accumulated_data)
-    df_events["address"] = df_events["address"].str.lower()
+    if "address" in df_events.columns:
+        df_events["address"] = df_events["address"].str.lower()
 
       
     return df_events, txs_event_not_decoded, unknown_event_addresses
